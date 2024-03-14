50b952c2c36d85634f794965b0015e3a5f005887
Holnyx <sarakomaichi@gmail.com>
2024-03-14 13:44:48 +0300

    Update 2024-03-14T10:44:38.270Z

--- src/js/modules/changeColorSelect.js
+++
@@ -1,6 +0,0 @@
-export const changeColor = () => {
-    const selectElement = document.getElementById('direction')
-    const selectedIndex = selectElement.selectedIndex
-    const selectedOption = selectElement.options[selectedIndex]
-    !selectedOption.disabled && selectElement.style.setProperty('color', '#1B1F2B', 'important')
-}
