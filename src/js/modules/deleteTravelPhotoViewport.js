8ea248ddf8f982f33b08fd8e0df63056f290a805
Holnyx <sarakomaichi@gmail.com>
2024-03-24 16:34:09 +0300

    Fixed errors with displaying pictures on different viewports in the Travel Photos section

--- src/js/modules/deleteTravelPhotoViewport.js
+++ src/js/modules/deleteTravelPhotoViewport.js
@@ -15,13 +15,18 @@ export const resize = () => {
     document.querySelectorAll('.images-travel__box').forEach(picture => {
         const images = picture.querySelectorAll('img.image-off');
         const imagesViewport = picture.querySelectorAll('img.image-off-viewport')
-        if (window.innerWidth <= 1024 && window.innerWidth > 768) {
-            removeImages(images)
-        } else if (window.innerWidth <= 768) {
+        if (window.innerWidth <= 1024 ) {
+            removeImages(images);
+        }
+        if (window.innerWidth <= 768) {
             removeImages(imagesViewport);
-        } else {
+        }
+        if (window.innerWidth > 768) {
             restoreImages(imagesViewport);
+        }
+        if (window.innerWidth > 1024) {
             restoreImages(images);
         }
+        
     });
 }
