ade28da96e77a2f86d4cfdeda2013c984c4c8fd7
Holnyx <sarakomaichi@gmail.com>
2024-04-02 13:06:48 +0300

    Removed !important, added mixins for device resolution, corrected the order of writing styles

--- src/js/modules/headerScrollChanges.js
+++ src/js/modules/headerScrollChanges.js
@@ -2,6 +2,7 @@ export const scrollHeader = () => {
     const header = document.getElementById('header')
     const headerClasses = Array.from(header.classList)
     const index = headerClasses.indexOf('show')
+    window.scrollY < 450 ? header.style.position = 'absolute' : header.style.position = ''
     window.scrollY >= 450 ? !headerClasses.includes('show') && headerClasses.push('show') : index !== -1 && headerClasses.splice(index, 1)
     header.className = headerClasses.join(' ')
 }
